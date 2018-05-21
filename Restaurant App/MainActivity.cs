7a9c85058e88f328576a0233fb574e098cfcfbd5
DanielBroere <37505331+DanielBroere@users.noreply.github.com>
2018-05-21 15:42:38 +0200

    Bezig met workshop

--- Restaurant App/MainActivity.cs
+++ Restaurant App/MainActivity.cs
@@ -3,7 +3,9 @@ using Android.Content.PM;
 using Android.OS;
 using Android.Support.V4.Widget;
 using Android.Views;
-
+using System.Linq;
+using System.Collections.Generic;
+using Android.Widget;
 using Restaurant_App.Fragments;
 using Android.Support.V7.App;
 using Android.Support.V4.View;
@@ -14,8 +16,83 @@ namespace Restaurant_App
     [Activity(Label = "@string/app_name", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, Icon = "@drawable/Icon")]
     public class MainActivity : AppCompatActivity
     {
+        private Button NewButton, UpdateButton, DeleteButton;
+        private ListView View;
+        private EditText Input;
+        private List<string> res = new List<string>();
+        private ArrayAdapter<string> adapter;
+        DB.DB db = new DB.DB();
 
-        DrawerLayout drawerLayout;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            db.createTable<Person>();
+
+            NewButton = FindViewById<Button>(Resource.Id.newButton);
+            UpdateButton = FindViewById<Button>(Resource.Id.UpdateButton);
+            DeleteButton = FindViewById<Button>(Resource.Id.DeleteButton);
+
+            List<buttonAction> Buttons = new List<buttonAction>();
+
+            Buttons.Add(new buttonAction(NewButton, (() =>
+            {
+                if (Input.Text != "")
+                {
+                    db.Insert(new Person(4, Input.Text));
+                }
+            })));
+
+            Buttons.Add(new buttonAction(UpdateButton, (() =>
+            {
+                string query = "";
+
+                IEnumerable<Person> Lijst;
+
+                if (Input.Text != "")
+                {
+                    Lijst = db.Select<Person>("SELECT * FROM Person WHERE Name =?", new[] { Input.Text });
+                }
+                else
+                {
+                    Lijst = db.Select<Person>("SELECT * FROM Person");
+                }
+
+                res.Clear();
+                foreach (var Item in Lijst.ToList())
+                {
+                    res.Add(Item.ToString());
+                }
+
+                adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, res);
+                View.Adapter = adapter;
+            })));
+
+            Buttons.Add(new buttonAction(DeleteButton, (() =>
+            {
+                if (Input.Text != "")
+                {
+                    db.Execute("DELETE FROM Person WHERE Name =?", Input.Text);
+                }
+                else
+                {
+                    db.Execute("DELETE FROM Person");
+                }
+            })));
+
+            View = FindViewById<ListView>(Resource.Id.ListView);
+
+            Input = FindViewById<EditText>(Resource.Id.Input);
+
+
+            foreach (var button in Buttons)
+            {
+                button.execute();
+            }
+            DrawerLayout drawerLayout;
         NavigationView navigationView;
 
         IMenuItem previousItem;
