e4435483c1c7245281e20f3080383010130a4160
IYIatthys <31695309+IYIatthys@users.noreply.github.com>
2018-05-29 11:28:01 +0200

    Filterscherm succes en lijst template met restaurants

--- Restaurant App/Fragments/Fragment2.cs
+++ Restaurant App/Fragments/Fragment2.cs
@@ -1,16 +1,22 @@
 using Android.OS;
 using Android.Support.V4.App;
 using Android.Views;
+using Android.Widget;
+using System.Collections.Generic;
 
 namespace Restaurant_App.Fragments
 {
     public class Fragment2 : Fragment
     {
+        private List<string> mItems;
+        private ListView mlistview;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
+            
         }
 
         public static Fragment2 NewInstance()
@@ -22,8 +28,21 @@ namespace Restaurant_App.Fragments
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            var ignored = base.OnCreateView(inflater, container, savedInstanceState);
-            return inflater.Inflate(Resource.Layout.fragment2, null);
+            //var ignored = base.OnCreateView(inflater, container, savedInstanceState);
+            base.OnCreateView(inflater, container, savedInstanceState);
+            var view = inflater.Inflate(Resource.Layout.fragment2, null);
+
+            mlistview = view.FindViewById<ListView>(Resource.Id.listView1);
+            mItems = new List<string>();
+            mItems.Add("Restaurant1");
+            mItems.Add("Restaurant2");
+            mItems.Add("Restaurant3");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(Activity, Android.Resource.Layout.SimpleListItem1, mItems);
+            mlistview.Adapter = adapter;
+
+            return view;
+            
         }
     }
 }
